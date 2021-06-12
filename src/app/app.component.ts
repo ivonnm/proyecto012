9dd9f97e0ef09c433fc34576cdad6893e7326a8e
ivonnm <66538222+ivonnm@users.noreply.github.com>
2021-06-11 19:30:17 -0500

    tarea

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticulosService } from './articulos.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  articulos = null;
+  title = 'proyecto012';
+  constructor (private articulosServicio: ArticulosService){
+
+  }
+  ngOnInit(){
+    this.articulos==this.articulosServicio.retornar();
+  }
+}
